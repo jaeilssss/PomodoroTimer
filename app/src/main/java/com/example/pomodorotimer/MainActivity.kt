9dd063af080982f8bee11c13333f9bd6c1018e4b
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-05-09 22:51:45 +0900

    5.9

--- app/src/main/java/com/example/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.pomodorotimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView : TextView by lazy {
+        findViewById<TextView>(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondeTextView: TextView by lazy {
+        findViewById<TextView>(R.id.remainSecondeTextView)
+    }
+
+    private val seekbar : SeekBar by lazy {
+        findViewById<SeekBar>(R.id.seekbar)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+
+    }
+
+    private fun bindViews(){
+        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    remainMinutesTextView.text = "%02d".format(p1)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
     }
 }
